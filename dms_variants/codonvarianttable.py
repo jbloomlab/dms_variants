cf88ac187be08c976109026e3b5c5d3507f7dfff
jbloom <jbloom@fredhutch.org>
2023-03-19 06:41:27 -0700

    add `primary_target_only` to `prob_escape`

--- dms_variants/codonvarianttable.py
+++ dms_variants/codonvarianttable.py
@@ -717,6 +717,7 @@ class CodonVariantTable:
         min_neut_standard_count=1e3,
         ceil_n_aa_substitutions=4,
         drop_neut_standard_target=True,
+        primary_target_only=False,
     ):
         r"""Compute probability of escape relative to a neutralization standard.
 
@@ -756,6 +757,9 @@ class CodonVariantTable:
         drop_neut_standard_target : bool
             Drop the neutralization standard variant-level results from the
             returned data frames.
+        primary_target_only : bool
+            Drop everything except the primary target and neutralization standard
+            target before beginning calculations.
 
         Returns
         -------
@@ -814,6 +818,10 @@ class CodonVariantTable:
         # get neut_standard fracs for each library / sample
         fracs = (
             self.n_variants_df(primary_target_only=False)
+            .query(
+                "(not @primary_target_only) "
+                "or (target in [@self.primary_target, @neut_standard_target])"
+            )
             .assign(
                 n=lambda x: x.groupby(["library", "sample"])["count"].transform("sum"),
                 frac=lambda x: x["count"] / x["n"],
@@ -851,7 +859,10 @@ class CodonVariantTable:
                     raise ValueError(f"neut standard {var} too low:\n{too_low}")
 
         # get variant counts grouped by `by`
-        count_df = self.variant_count_df
+        count_df = self.variant_count_df.query(
+            "(not @primary_target_only) "
+            "or (target in [@self.primary_target, @neut_standard_target])"
+        )
         group_cols = [
             "codon_substitutions",
             "n_codon_substitutions",
@@ -3025,7 +3036,6 @@ class CodonVariantTable:
         for lib, sample in itertools.product(
             df["library"].unique().tolist(), df["sample"].unique().tolist()
         ):
-
             i_df = df.query("library == @lib & sample == @sample")
             if len(i_df) == 0:
                 continue  # no data for this library and sample
